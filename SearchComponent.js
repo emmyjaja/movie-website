cc38fa488fd9e115740c35208dd9d9d8156e30f3
emmyjaja <emmykjohn@gmail.com>
2024-01-10 17:47:59 +0600

    Add new files

---
+++ SearchComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const SearchComponent = () => {
+  return (
+    <div>
+    <FontAwesomeIcon icon={faSearch} />
+    {/* Your other search component code */}
+  </div>
+  )
+}
+
+export default SearchComponent
